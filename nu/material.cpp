65d9d84da7bbfc85436d4b9b082315ddfaa4faaa
RatcheT2497 <DarkHerobrine2477@gmail.com>
2024-06-25 22:08:29 +0200

    [nu] Fleshed out Material class a bit using Xhaos' decompilation as a base.

--- nu/material.cpp
+++ nu/material.cpp
@@ -1,29 +1,42 @@
 #include "material.h"
 namespace Nu {
-
+    
+    Material::Material() {}
     bool Material::read(BinaryStream& stream)
     {
-        stream.seek(SEEK_CUR, 20);
-        auto diffuse_r = stream.read_float();
-        if (!diffuse_r.has_value())
+        stream.seek(SEEK_CUR, 4);
+        auto newval = stream.read_word();
+        if (!newval.has_value())
             return false;
-        auto diffuse_g = stream.read_float();
-        if (!diffuse_r.has_value())
+        
+        union { Attributes attr; uint32_t val; } cursed;
+        
+        // hee hoo bitwise bangin goes brrr
+        cursed.val = newval.value();
+        m_attributes = cursed.attr;
+
+        if (!m_ambient.read(stream))
             return false;
-        auto diffuse_b = stream.read_float();
-        if (!diffuse_r.has_value())
+        if (!m_diffuse.read(stream))
             return false;
 
-        stream.seek(SEEK_CUR, 24);
+        stream.seek(SEEK_CUR, 16);
+
+        auto power_raw = stream.read_float();
+        if (!power_raw.has_value()) 
+            return false;
+
+        auto alpha_raw = stream.read_float();
+        if (!alpha_raw.has_value()) 
+            return false;
 
         auto texture_id = stream.read_word();
         if (!texture_id.has_value())
             return false;
 
         stream.seek(SEEK_CUR, 24);
-        m_diffuse_r = diffuse_r.value();
-        m_diffuse_g = diffuse_g.value();
-        m_diffuse_b = diffuse_b.value();
+        m_power = power_raw.value();
+        m_alpha = alpha_raw.value();
         m_texture = texture_id.value();
         return true;
     }
--- nu/material.h
+++ nu/material.h
@@ -2,16 +2,43 @@
 #ifndef MATERIAL_H
 #define MATERIAL_H
 #include "../binary_stream.h"
+#include "../math/vec4.h"
 namespace Nu {
-
+    // Source: https://github.com/Open-Travelers/OpenCrashWOC/blob/main/code/src/nu3dx/nu3dxtypes.h
+    struct Attributes
+    {
+        unsigned int alpha : 2; // Offset: 0x0, Bit Offset: 0, Bit Size: 2
+        unsigned int filter : 2; // Offset: 0x0, Bit Offset: 2, Bit Size: 2
+        unsigned int fx : 2; // Offset: 0x0, Bit Offset: 4, Bit Size: 2
+        unsigned int utc : 2; // Offset: 0x0, Bit Offset: 6, Bit Size: 2
+        unsigned int vtc : 2; // Offset: 0x0, Bit Offset: 8, Bit Size: 2
+        unsigned int cull : 2; // Offset: 0x0, Bit Offset: 10, Bit Size: 2
+        unsigned int zmode : 2; // Offset: 0x0, Bit Offset: 12, Bit Size: 2
+        unsigned int lighting : 2; // Offset: 0x0, Bit Offset: 14, Bit Size: 2
+        unsigned int colour : 1; // Offset: 0x0, Bit Offset: 16, Bit Size: 1
+        unsigned int fill : 1; // Offset: 0x0, Bit Offset: 17, Bit Size: 1
+        unsigned int atst : 3; // Offset: 0x0, Bit Offset: 18, Bit Size: 3
+        unsigned int aref : 8; // Offset: 0x0, Bit Offset: 21, Bit Size: 8
+        unsigned int afail : 2; // Offset: 0x0, Bit Offset: 29, Bit Size: 2
+        unsigned int uvmode : 1; // Offset: 0x0, Bit Offset: 31, Bit Size: 1
+    };
     class Material {
     public:
+        Material();
         bool read(BinaryStream& stream);
+        Attributes attributes() { return m_attributes; }
+        Vec3 &diffuse() { return m_diffuse; }
+        Vec3 &ambient() { return m_ambient; }
+        float power() { return m_power; }
+        float alpha() { return m_alpha; }
+        int texture() { return m_texture; }
     protected:
-        int m_texture;
-        float m_diffuse_r;
-        float m_diffuse_g;
-        float m_diffuse_b;
+        float m_power { 1.0f };
+        float m_alpha { 1.0f };
+        int m_texture { -1 };
+        Vec3 m_ambient { Vec3(1.f, 1.f, 1.f) };
+        Vec3 m_diffuse { Vec3(1.f, 1.f, 1.f) };
+        Attributes m_attributes { 0 };
     };
 
 }
