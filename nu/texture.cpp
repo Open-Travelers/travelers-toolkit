bd5177585b63403098f16215dcb58c828ccdf90a
RatcheT2497 <DarkHerobrine2477@gmail.com>
2024-06-25 22:09:25 +0200

    [nu] Added preliminary Texture class reading. Data reading is destructive; will need a rewrite.

--- nu/texture.cpp
+++ nu/texture.cpp
@@ -1,19 +1,38 @@
 #include "texture.h"
+#include <cstring>
 #include <iostream>
+#include <cstdlib>
 namespace Nu {
-    static const int texture_size(int type, int width, int height, int pixlength)
+    static const int pixel_size(int type)
     {
-        static constexpr int sizes[] = { 0x10, 0x10, 0x18, 0x20, 4, 8, 0 };
-        
-        int size = width * height;
-        if (type >= 0 && type <= 5)
-            size *= (sizes[type] >> 3);
-        else size = 0;
-
-        for (int i = 0; i < pixlength; i++)
+        switch (type)
         {
-            int additional = sizes[type];
-            size += (width >> (i & 0x1f)) * (height >> (i & 0x1f)) * (additional >> 3);
+            case 0:
+            case 1:
+                return 0x10;
+            case 2:
+                return 0x18;
+            case 3:
+                return 0x20;
+            case 4:
+                return 4;
+            case 5:
+                return 8;
+            default:
+                return 0;
+        }
+    }
+    static const int texture_size_mm(int type, int width, int height, int mipmaps)
+    {
+        int size = width * height * pixel_size(type) >> 3;
+        if (mipmaps > 0)
+        {
+            int i = 0;
+            do {
+                i++;
+                int additional = pixel_size(type);
+                size = size + ((width >> (i & 0x1f)) * (height >> (i & 0x1f)) * additional >> 3);
+            } while (i < mipmaps);
         }
         return size;
     }
@@ -35,20 +54,73 @@ namespace Nu {
         if (mipmap_count_value == 1) 
             mipmap_count_value = 0;
 
-        int size = texture_size(type.value(), width.value(), height.value(), mipmap_count_value);
-        stream.seek(SEEK_CUR, size);
+        int size = texture_size_mm(type.value(), width.value(), height.value(), mipmap_count_value);
+
+        uint8_t *txdata = new uint8_t[size];
+        stream.read(size, txdata);
         
+        m_type = type.value();
+        m_width = width.value();
+        m_height = height.value();
+
         int palette_size = 0;
-        if (type.value() == 4)
+        if (m_type == 4)
             palette_size = 0x40;
-        else if (type.value() == 5)
+        else if (m_type == 5)
             palette_size = 0x400;
 
-        if (palette_size != 0)
-            stream.seek(SEEK_CUR, palette_size);
+        m_rgba_data.reserve(m_width * m_height * 4);
+        if (m_type == 4 || m_type == 5)
+        {
+            uint8_t *paldata = new uint8_t[palette_size];
+            stream.read(palette_size, (uint8_t*) paldata);
+            if (m_type == 4)
+                size *= 2;
+            for (int i = 0; i < size; i++)
+            {
+                uint8_t idx;
+                if (type.value() == 4)
+                {
+                    if ((i & 1) == 0)
+                    {
+                        idx = txdata[i >> 1] >> 4;
+                    } else {
+                        idx = txdata[i >> 1] & 0xf;
+                    }
+                } else if (type.value() == 5)
+                {
+                    idx = txdata[i];
+                }
+                
+                uint8_t a = paldata[idx * 4];
+                uint8_t r = paldata[idx * 4 + 1];
+                uint8_t g = paldata[idx * 4 + 2];
+                uint8_t b = paldata[idx * 4 + 3];
+                m_rgba_data.push_back(b);
+                m_rgba_data.push_back(g);
+                m_rgba_data.push_back(r);
+                m_rgba_data.push_back(a);
+            }
+            delete[] paldata;
+        } else if (m_type == 3)
+        {
+            for (int i = 0; i < size; i+=4)
+            {
+                uint8_t a = txdata[i];
+                uint8_t r = txdata[i + 1];
+                uint8_t g = txdata[i + 2];
+                uint8_t b = txdata[i + 3];
+                m_rgba_data.push_back(b);
+                m_rgba_data.push_back(g);
+                m_rgba_data.push_back(r);
+                m_rgba_data.push_back(a);
+            }
+        }
+        delete[] txdata;
 
-        std::cout << "Texture of type " << std::to_string(type.value()) << " of width " << std::to_string(width.value()) << " and height " << std::to_string(height.value()) << ". Mipmaps: " << std::to_string(mipmap_count.value()) << std::endl;
+
+        std::cout << "Texture of type " << std::to_string(type.value()) << " of width " << std::to_string(width.value()) << " and height " << std::to_string(height.value()) << ". Mipmaps: " << std::to_string(mipmap_count.value()) << ", palette: " << std::to_string(palette_size) << ", tex size: " << std::to_string(m_rgba_data.size()) << std::endl;
         return true;
     }
-
+    Texture::Texture() {}
 }
--- nu/texture.h
+++ nu/texture.h
@@ -7,10 +7,17 @@ namespace Nu {
     class Texture
     {
     public:
+        Texture();
         bool read(BinaryStream& stream);
+        uint8_t *get_rgba_data() { return m_rgba_data.data(); }
+        int get_rgba_data_size() { return m_rgba_data.size(); }
+        int width() { return m_width; }
+        int height() { return m_height; }
     protected:
+        int m_type { 0 };
         int m_width { 0 };
         int m_height { 0 };
+        std::vector<uint8_t> m_rgba_data;
     };
 
 }
