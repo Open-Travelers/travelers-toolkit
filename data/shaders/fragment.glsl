ec93214d666c992a355e900235ff71e659ef5c7e
RatcheT2497 <ratchetnumbers@proton.me>
2025-07-20 23:00:46 +0200

    CMake cleanup

---
+++ data/shaders/fragment.glsl
@@ -0,0 +1,18 @@
+#version 330 core
+out vec4 o_color;
+in vec4 v_color;
+in vec2 v_uv;
+in vec3 v_normal;
+
+uniform float u_alpha;
+uniform vec3 u_diffuse;
+uniform vec3 u_ambient;
+uniform float u_ambient_power;
+
+uniform sampler2D u_texture;
+
+void main() {
+    vec4 ambient = u_ambient_power * u_ambient;
+    vec4 final_light_color = ambient;
+    o_color = final_light_color * texture(u_texture, v_uv) * v_color;
+}
---
+++ data/shaders/vertex.glsl
@@ -0,0 +1,30 @@
+#version 330 core
+layout (location=0) in vec3 l_position;
+layout (location=1) in vec3 l_normal;
+layout (location=2) in vec2 l_uv;
+layout (location=3) in uint l_color;
+
+uniform float u_backdrop_color_usage;
+uniform mat4 u_mvp;
+out vec4 v_color;
+out vec2 v_uv;
+out vec3 v_normal;
+
+const vec4 WHITE = vec4(1.0, 1.0, 1.0, 1.0);
+
+vec4 split_color(uint color)
+{
+    vec4 result;
+    result.r = ((color >> 8) & uint(0xff)) / 256.0;
+    result.g = ((color >> 16) & uint(0xff)) / 256.0;
+    result.b = ((color >> 24) & uint(0xff)) / 256.0;
+    result.a = float(color & uint(0xff));
+    return result;
+}
+
+void main() {
+    vec4 color = mix(split_color(l_color), WHITE, u_backdrop_color_usage);
+    v_uv = l_uv;
+    v_normal = l_normal;
+    gl_Position = u_mvp * vec4(l_position, 1.0f);
+}
